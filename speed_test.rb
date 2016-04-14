8c238a0f22fc94f50cc519fce21639a7e9cb1722
upstateruby@example.com <upstateruby@example.com>
2016-04-14 01:17:26 +0000

    fixing that jam

--- speed_test.rb
+++ speed_test.rb
@@ -1,10 +1,16 @@
 require 'benchmark'
+require 'net/http'
+require 'uri'
 
 time = Benchmark.measure do
 `curl -o /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip`
 end
 
-total = 11*1024/time.real
-
+total = 11/time.real*8
+p total
+#req = Net::HTTP::Put.new('/properties/1', 
+#req.body = 
+#response = Net::HTTP.new('http://openworks-status-api.herokuapp.com',80).start { |http| http.request(##req) }
 #write to open works status api
-#curl -H "Content-Type: application/json" -H "Authorization: Token openworksauthtoken" https://openworks-status-api.herokuapp.com/things
+`curl --request PATCH --data '{"data": {"type":"properties", "id": "1", "attributes": {"value": #{total} }}}' --header "Content-Type: application/vnd.api+json" --header "Authorization: Token openworksauthtoken" http://openworks-status-api.herokuapp.com/properties/1.json`
+
